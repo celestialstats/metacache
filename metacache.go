43153902d8348394a9da481366b5e7c18cf6f71e
Caesar Kabalan <caesar.kabalan@gmail.com>
2017-04-15 23:47:20 -0700

    Add locking around the actual cache data.

--- metacache.go
+++ metacache.go
@@ -1,16 +1,20 @@
 package metacache
 
 import (
+	"errors"
+	"strconv"
+	"sync"
+	"time"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/davecgh/go-spew/spew"
-	"strconv"
-	"time"
 )
 
 type MetaCache struct {
 	data          map[string]map[string]string
 	updateChannel chan map[string]MetaLookup
 	ValidMinutes  int
+	dataLock      *sync.Mutex
 }
 
 func NewMetaCache(ValidMinutes, MaxQueue int) *MetaCache {
@@ -18,27 +22,27 @@ func NewMetaCache(ValidMinutes, MaxQueue int) *MetaCache {
 	mc.ValidMinutes = ValidMinutes
 	mc.data = make(map[string]map[string]string)
 	mc.updateChannel = make(chan map[string]MetaLookup, MaxQueue)
+	mc.dataLock = &sync.Mutex{}
 	go mc.update()
 	return mc
 }
 
 func (cache *MetaCache) CheckAndUpdate(key string, lookup MetaLookup) {
+	cache.dataLock.Lock()
+	defer cache.dataLock.Unlock()
 	if _, ok := cache.data[key]; ok {
 		log.Debug("Key ", key, " exists. Checking if still valid...")
 		if _, ok := cache.data[key]["Updated"]; ok {
-			log.Debug("\tHas Updated Attribute...")
 			cacheTime, _ := strconv.ParseInt(cache.data[key]["Updated"], 10, 64)
 			curTime := time.Now().UTC().Unix()
 			olderInvalid := curTime - int64(cache.ValidMinutes*60)
 			if olderInvalid > cacheTime {
-				log.Debug("\t\tOut of Date...")
+				log.Debug("\tOut of Date...")
 			} else {
-				log.Debug("\t\tUp To Date (", (curTime - cacheTime), "s old)...")
+				log.Debug("\tUp To Date (", (curTime - cacheTime), "s old)...")
 				// We're up to date so return early...
 				return
 			}
-		} else {
-			log.Debug("\tMissing Updated Attribute...")
 		}
 	} else {
 		log.Debug("Key ", key, " does not exist.")
@@ -51,14 +55,27 @@ func (cache *MetaCache) CheckAndUpdate(key string, lookup MetaLookup) {
 }
 
 func (cache *MetaCache) PrintData() {
+	cache.dataLock.Lock()
 	spew.Dump(cache.data)
+	cache.dataLock.Unlock()
+}
+
+func (cache *MetaCache) Retrieve(key string) (returnMap map[string]string, err error) {
+	cache.dataLock.Lock()
+	defer cache.dataLock.Unlock()
+	if theMap, ok := cache.data[key]; ok {
+		return theMap, nil
+	}
+	return nil, errors.New("Key not found.")
 }
 
 func (cache *MetaCache) update() {
 	for theMap := range cache.updateChannel {
 		for key, lookup := range theMap {
+			cache.dataLock.Lock()
 			cache.data[key] = lookup.Function(lookup.Parameters)
 			cache.data[key]["Updated"] = strconv.FormatInt(time.Now().UTC().Unix(), 10)
+			cache.dataLock.Unlock()
 		}
 		// Eventually push this up to the database.
 	}
